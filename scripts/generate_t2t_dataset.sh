775193c7902af2c91328b0a179053bf0f26377c7
ajdsouza <aj.anthonydsouza@gmail.com>
2026-03-06 02:35:35 +0100

    preprocessing script ready

---
+++ scripts/generate_t2t_dataset.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+
+uv run scripts/generate_t2t_dataset.py \
+    --dataset_name='dvgodoy/yoda_sentences' \
+    --flip_ratio=0.5 \
+    --num_proc=2 \
+    --src_column='sentence' \
+    --tgt_column='translation' \
+    --train_test_ratio=0.1 \
+    --max_length=128 \
+    --tokenizer='Qwen/Qwen2.5-0.5B' \
+    --data_dir'=data/'
